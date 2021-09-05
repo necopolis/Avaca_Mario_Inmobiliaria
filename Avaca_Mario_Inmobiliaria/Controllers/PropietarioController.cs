f9824d1fbe66ca623fb7b12ba04c43c55e98316c
necopolis <39461844+necopolis@users.noreply.github.com>
2021-09-04 21:39:12 -0300

    Cambio de carteles a español, agreado funciones que faltaban a las vistas y contrato funcionando

--- Avaca_Mario_Inmobiliaria/Controllers/PropietarioController.cs
+++ Avaca_Mario_Inmobiliaria/Controllers/PropietarioController.cs
@@ -22,7 +22,8 @@ namespace Avaca_Mario_Inmobiliaria.Controllers
         // GET: PropietarioController
         public ActionResult Index()
         {
-            var lista = data.ObtenerTodos(); 
+            var lista = data.ObtenerTodos();
+           ViewBag.Message = TempData["Message"];
             return View(lista);
         }
 
@@ -46,13 +47,27 @@ namespace Avaca_Mario_Inmobiliaria.Controllers
         {
             try
             {
-                data.Alta(propietario);
-                return RedirectToAction(nameof(Index));
+                var res = data.Alta(propietario);
+                if (res > 0)
+                {
+                    ViewBag.Message = "Propietario Agregado con Exito";
+                    ViewBag.Message = "BAG MENSAJE Logrado con exito";
+                    ViewBag.Error = "BAG ERROR Logrado sin exito";
+                    TempData["Message"] = "TEMPO DATA Logrado con exito";
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.Error = @"No se ha podido Agregar el propietario" 
+                                    + propietario.Apellido + @"Intentelo mas tarde o realice el reclamo a servicio tecnico";
+                    return View();
+                }
+                
             }
             catch (Exception ex)
             {
-                ViewBag.Error = ex.Message;
-                ViewBag.StackTrate = ex.StackTrace;
+                ViewBag.Error = @"No se ha podido Agregar el propietario, 
+                                se ha producido algun tipo de error, realice el reclamo a servicio tecnico";
                 return View();
             }
         }
@@ -71,13 +86,26 @@ namespace Avaca_Mario_Inmobiliaria.Controllers
         {
             try
             {
-                data.Modificar(id, propietario);
-                return RedirectToAction(nameof(Index));
+                var res = data.Modificar(id, propietario);
+                if (res>0)
+                {
+                    TempData["Message"] = "Logrado con exito";
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.Error = @"No se ha podido Editar el propietario"
+                                    + propietario.Apellido + "Intentelo mas tarde o realice el reclamo a servicio tecnico";
+                    return View();
+                }
+                
             }
             catch (Exception ex)
             {
-                ViewBag.Error = ex.Message;
-                ViewBag.StackTrate = ex.StackTrace;
+                //ViewBag.Error = ex.Message;
+                //ViewBag.StackTrate = ex.StackTrace;
+                ViewBag.Error = @"No se ha podido Editar el propietario, 
+                                se ha producido algun tipo de error, realice el reclamo a servicio tecnico";
                 return View();
             }
         }
@@ -96,13 +124,26 @@ namespace Avaca_Mario_Inmobiliaria.Controllers
         {
             try
             {
-                data.Baja(id);
-                return RedirectToAction(nameof(Index));
+                var res = data.Baja(id);
+                if (res>0)
+                {
+                    ViewBag.Message = "Propietario Eliminado con Exito";
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.Error = @"No se ha podido Eliminar el propietario
+                                    Intentelo mas tarde o realice el reclamo a servicio tecnico";
+                    return View();
+                }
+                
             }
             catch (Exception ex)
             {
-                ViewBag.Error = ex.Message;
-                ViewBag.StackTrate = ex.StackTrace;
+                //ViewBag.Error = ex.Message;
+                //ViewBag.StackTrate = ex.StackTrace;
+                ViewBag.Error = @"No se ha podido Eliminar el propietario, 
+                                se ha producido algun tipo de error, realice el reclamo a servicio tecnico";
                 return View();
             }
         }
