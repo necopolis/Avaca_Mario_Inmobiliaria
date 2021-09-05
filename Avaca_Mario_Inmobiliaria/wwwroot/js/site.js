13b267050c026839b816575f46227c2a790f4537
necopolis <39461844+necopolis@users.noreply.github.com>
2021-09-04 21:46:32 -0300

    Probe dispara un hover con referencia a site.js
    Puse una palabra con un id en el html y coloque un disparador en el JS de site que viene en el layout el cual hace disparar un ALERT

--- Avaca_Mario_Inmobiliaria/wwwroot/js/site.js
+++ Avaca_Mario_Inmobiliaria/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function editar(x) {
+    //#editarModal==id="editarModal"(Modal) .modal(show) para que se vea
+
+    $(document).ready(function () {
+        $("#p1").hover(function () {
+            alert("You entered p1!");
+        },
+            function () {
+                alert("Bye! You now leave p1!");
+            });
+    });
+    
+    
+}
+$(document).ready(function () {
+    $("#p1").hover(function () {
+        alert("You entered p1!");
+    },
+        function () {
+            alert("Bye! You now leave p1!");
+        });
+});
