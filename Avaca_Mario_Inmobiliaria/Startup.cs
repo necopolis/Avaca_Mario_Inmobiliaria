c2082103a0d32d76a8a0f2a105ac7b2e7793e346
necopolis <39461844+necopolis@users.noreply.github.com>
2021-09-20 21:43:59 -0300

    Despliegue e implementacion de Usuario, implementacion de login, correcciones de la integridad del modelo y correcciones menores
    Se creo usuario, BD, Model, Controllers, Data.
    Se reviso la integridad que al llamar un inquilino en contratos no este eliminado, que el inmueble no este alquilado o dado de baja, etc

--- Avaca_Mario_Inmobiliaria/Startup.cs
+++ Avaca_Mario_Inmobiliaria/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,18 @@ namespace Avaca_Mario_Inmobiliaria
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>//el sitio web valida con cookie
+                {
+                    options.LoginPath = "/Usuario/Login";
+                    options.LogoutPath = "/Usuario/Logout";
+                    options.AccessDeniedPath = "/Home/Restringido";
+                });
+            services.AddAuthorization(options =>
+            {
+                //options.AddPolicy("Empleado", policy => policy.RequireClaim(ClaimTypes.Role, "Administrador", "Empleado"));
+                options.AddPolicy("Administrador", policy => policy.RequireRole("Administrador", "SuperAdministrador"));
+            });
             services.AddControllersWithViews();
         }
 
@@ -44,6 +57,7 @@ namespace Avaca_Mario_Inmobiliaria
 
             app.UseRouting();
 
+            app.UseAuthorization();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
